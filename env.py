2d90bf1f78cbea67e0277da5b1736b5ed29e533b
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-08 13:48:55 +0000

    Add: Database Tables

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "<postgres://uspo8gggse3:P5Z5pRhSYh8o@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/romp_vegan_hut_429921>")
+    "DATABASE_URL", "postgres://uspo8gggse3:P5Z5pRhSYh8o@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/romp_vegan_hut_429921"
+)
