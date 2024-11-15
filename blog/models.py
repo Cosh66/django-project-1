bf1dadb05938a77b3ea3484cc1d16d125748c561
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-15 19:20:56 +0000

    No comment

--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Post(models.Model):
-    # Assuming a Post model exists with at least a title and content
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     content = models.TextField()
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name='comments'
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='commenter'
-    )
-    body = models.TextField()
-    approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'Comment by {self.author} on {self.post}'
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    field_2 = models.IntergerField(default='Hello, World!')
+    field_3 = models.CharField(null=True)
+    
    
