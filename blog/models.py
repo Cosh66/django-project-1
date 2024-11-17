83d4719128eb4f07fca5ca5d17dd2d48a9b8dfa3
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-17 16:01:20 +0000

    Add: home.html

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -11,5 +15,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
    
+
+
     
    
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Example view
+    path('', views.home, name='home'),
 ]
 
