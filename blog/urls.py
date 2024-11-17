c32e180facee78f6cdb1a7622ac64fc47ea999f5
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-17 22:46:47 +0000

    no comment

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import, get_object_or_404
 
 def home(request):
     return render(request, 'home.html')
@@ -7,4 +7,27 @@ def home(request):
 
 template_name = "blog/index.html"
 paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
 
