a9a32ef3266064cdb39eafd23cf8878de7537585
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-18 14:22:09 +0000

    Bla

--- env.py
+++ env.py
@@ -3,3 +3,6 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://uspo8gggse3:P5Z5pRhSYh8o@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/romp_vegan_hut_429921"
 )
+
+os.environ.setdefault(
+    "SECRET_KEY", 'a^&ow-r$btgt#@1oi+=yblghqs&fi)stxwy)i9_2ib=bq#64&w')
--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e12qo^+t)&s(8561jnu#2%jqvooro1ue*&hmf-(0z90*h%1_^l'
+SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
