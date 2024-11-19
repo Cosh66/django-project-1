f7d750aae317695e2961e21d391487dacf55027d
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-19 16:20:14 +0000

    Bla

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,6 @@
+from . import views
 from django.urls import path
-from .views import PostList, post_detail
-
 urlpatterns = [
-    path('', PostList.as_view(), name='post_list'),  # Home page: List of posts
-    path('<slug:slug>/', post_detail, name='post_detail'),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
-
-
