58d878ab25372640e22433e8f46c7083fd136155
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-19 16:15:44 +0000

    bla

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,24 @@
-from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Post
-
-# Home page: List of blog posts
-class PostList(ListView):
-    model = Post
-    template_name = 'post_list.html'  # Points to the template for the home page
-    context_object_name = 'posts'    # Name used in the template to loop through posts
-
-# Post detail page: Single blog post
+# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
-
     **Context**
-
     ``post``
         An instance of :model:`blog.Post`.
-
     **Template:**
-
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
     return render(
         request,
         "blog/post_detail.html",
         {"post": post},
     )
-
-
