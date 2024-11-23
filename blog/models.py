8b745749085c70fecb98b7f2b92174f9efd320ad
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-23 11:17:13 +0000

    Add: css, style.css to statics

--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-STATUS = [
-    (0, 'Draft'),
-    (1, 'Published'),
-]
+STATUS = ((0, "Draft"), (1, "Published"))
 
+# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     content = models.TextField()
-    status = models.IntegerField(choices=STATUS, default=0)
-    created_on = models.DateTimeField(auto_now_add=True)  # Ensure this matches your code
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ["-created_on"]
 
     def __str__(self):
-        return self.title
-   
+        return f"{self.title} | written by {self.author}"
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:  # Corrected to uppercase 'M'
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.body} | by {self.author}"
 
 
     
