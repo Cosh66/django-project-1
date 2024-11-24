f1c4d17267600cbe82a10555c7bf0813dda69505
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-24 18:38:34 +0000

    Add: Logo

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,9 @@ def post_detail(request, slug):
         {"post": post},
     )
 
-   from django.shortcuts import render
+from django.shortcuts import render  # This line must not be indented
 
 def about(request):
-    return render(request, 'blog/about.html')  
+    return render(request, 'blog/about.html')
+def home(request):
+    return render(request, 'blog/index.html')
