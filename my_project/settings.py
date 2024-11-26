178cdf37ae8e37b836231122ca1f68421bdafd51
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-11-26 19:53:08 +0000

    Add:account to templates

--- my_project/settings.py
+++ my_project/settings.py
@@ -171,3 +171,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # Global static folder
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_FORMS = {
+    'signup': 'blog.signup.html'
+}
