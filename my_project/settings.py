8699c0e7484f183fb9121716b499301743211e64
Yvonne Mccosh <yvonnemccosh1966@gmail.com>
2024-12-02 13:31:07 +0000

    Add: signup.css

--- my_project/settings.py
+++ my_project/settings.py
@@ -133,13 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-# Directory where static files will be collected for deployment
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user-uploaded files) configuration
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
